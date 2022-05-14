11d5b52eaec3da3025126a1dece935a5db41b196
fuad manye / <fahadbasi333@gmail.com>
2022-05-14 15:50:28 +0300

    deployment

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config:
 
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("://", "ql://", 1)  
+    SQLALCHEMY_DATABASE_URI = 'postgresql://etujabyelnapni:b9c669f5b422bd5ecaf5c5d1b2525a6c4486aca6104c1b3ebe824f3f69b0f223@ec2-54-80-123-146.compute-1.amazonaws.com:5432/ddvmm40s6dnpbr'  
     SECRET_KEY = os.environ.get('SECRET_KEY')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
