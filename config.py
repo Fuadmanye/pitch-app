78ce7222f836e5e4118661c9cef207b6a4f8cbb0
fuad manye / <fahadbasi333@gmail.com>
2022-05-16 13:04:18 +0300

    deployment

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ class Config:
 
    UPLOADED_PHOTOS_DEST ='app/static/photos' 
    SECRET_KEY = os.environ.get('SECRET_KEY')
-   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://fahadbasi:333@localhost/pitch' 
-   DEBUG = True
+#    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://fahadbasi:333@localhost/pitch' 
+  
 
 #    email configurations
    MAIL_SERVER = 'smtp.googlemail.com'
@@ -15,13 +15,9 @@ class Config:
 
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://etujabyelnapni:b9c669f5b422bd5ecaf5c5d1b2525a6c4486aca6104c1b3ebe824f3f69b0f223@ec2-54-80-123-146.compute-1.amazonaws.com:5432/ddvmm40s6dnpbr' 
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+      
+      SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("://", "ql://", 1)  
+   
     
     
 
